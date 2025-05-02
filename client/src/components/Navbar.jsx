2bf99f53c4c4de193ff49cd344138cfeb60e7d9f
P4NCH1T0 <98226655+P4NCH1T0@users.noreply.github.com>
2025-05-01 21:06:01 -0600

    router dom

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import{Link, Route, Routes} from 'react-router-dom'
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <nav className='nav'>
+      <a href='/' className='title'>titulo</a>
+      <ul>
+        <li>
+          <a href="/proyects">Proyectos</a>
+        </li>
+        <li>
+          <a href="/trayectory">Trayectoria</a>
+        </li>
+        <li>
+          <a href="/about">Sobre mi</a>
+        </li>
+        <li>
+          <a href="/contact">Contacto</a>
+        </li>
+      </ul>
+    </nav>
   )
 }
 
